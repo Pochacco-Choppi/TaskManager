0eb5e761b8ced1a567f250998e90a8f62ddd998c
Jaroslaw Miskiewicz <j.miskiewicz.ai@gmail.com>
2023-04-03 21:22:51 +0300

    feat: create Task model

---
+++ main/models/task.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from .user import User
+from .tag import Tag
+
+
+class Task(models):
+    class Status(models.TextChoices):
+        NEW = "new_task"
+        IN_DEVELOPMENT = "in_development"
+        IN_QA = "in_qa"
+        IN_CODE_REVIEW = "in_code_review"
+        READY_FOR_RELEASE = "ready_for_release"
+        RELEASED = "released"
+        ARCHIVED = "archived"
+
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL)
+    assignee = models.ForeignKey(User, on_delete=models.SET_NULL)
+    creation_date = models.DateField(auto_now_add=True)
+    change_date = models.DateField(auto_now=True)
+    deadline_date = models.DateField()
+    status = models.CharField(
+        max_length=255, default=Status.NEW, choices=Status.choices
+    )
+    priority = models.FloatField()
+    tags = models.ForeignKey(Tag, on_delete=models.SET_NULL)
