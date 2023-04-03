40b08c79c5cecc5f2eb6a6ce446257026bbd1e63
Jaroslaw Miskiewicz <j.miskiewicz.ai@gmail.com>
2023-04-03 19:12:17 +0300

    feat: add role field for User model

--- main/models/user.py
+++ main/models/user.py
@@ -1,5 +1,13 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    ...
+    class Roles(models.TextChoices):
+        DEVELOPER = "developer"
+        MANAGER = "manager"
+        ADMIN = "admin"
+
+    role = models.CharField(
+        max_length=255, default=Roles.DEVELOPER, choices=Roles.choices
+    )
