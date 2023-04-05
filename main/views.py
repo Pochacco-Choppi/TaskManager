f880c8fc0f03427d587557e740a4ce757521e57f
Jaroslaw Miskiewicz <j.miskiewicz.ai@gmail.com>
2023-04-06 02:25:42 +0300

    feat: add filters for api views

--- main/views.py
+++ main/views.py
@@ -1,12 +1,36 @@
 from rest_framework import viewsets
+import django_filters
 
 from .models import User, Task, Tag
 from .serializers import UserSerializer, TaskSerializer, TagSerializer
 
 
+class UserFilter(django_filters.FilterSet):
+    first_name = django_filters.CharFilter(lookup_expr="icontains")
+
+    class Meta:
+        model = User
+        fields = ("first_name",)
+
+
+class TaskFilter(django_filters.FilterSet):
+    status = django_filters.ChoiceFilter(choices=Task.Status.choices)
+    author = django_filters.ModelChoiceFilter(queryset=User.objects.all())
+    assignee = django_filters.ModelChoiceFilter(queryset=User.objects.all())
+    tags = django_filters.ModelMultipleChoiceFilter(
+
+        queryset=Tag.objects.all(),
+    )
+
+    class Meta:
+        model = Task
+        fields = ("status", "author", "tags", "assignee")
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.order_by("id")
     serializer_class = UserSerializer
+    filterset_class = UserFilter
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -16,6 +40,7 @@ class TaskViewSet(viewsets.ModelViewSet):
         .order_by("priority")
     )
     serializer_class = TaskSerializer
+    filterset_class = TaskFilter
 
 
 class TagViewSet(viewsets.ModelViewSet):
